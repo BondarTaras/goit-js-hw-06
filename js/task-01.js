a65e1233136e6c695f580db4d4af4e4b32200dba
Taras Bondar <tarasbondar2@gmail.com>
2023-05-18 20:21:34 +0300

    add

--- js/task-01.js
+++ js/task-01.js
@@ -1,22 +1,15 @@
-const categoriesList = document.querySelector("#categories");
-const categories = categoriesList.querySelectorAll(".item");
 
-console.log(`Number of categories: ${categories.length}`);
 
-categories.forEach((category) => {
+const categoriesList = document.getElementById("categories").childNodes;
+const categoryItems = Array.from(categoriesList).filter(
+  (node) => node.nodeType === Node.ELEMENT_NODE
+);
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach((category) => {
   const categoryName = category.querySelector("h2").textContent;
   const categoryElements = category.querySelectorAll("li");
-
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
-
-//!------------------------------------------------
-const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
-];
--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,9 @@
 const fontSizeControl = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
+const initialFontSize = fontSizeControl.value + "px";
+text.style.fontSize = initialFontSize;
+
 fontSizeControl.addEventListener("input", () => {
   const fontSize = fontSizeControl.value + "px";
   text.style.fontSize = fontSize;
